1b7cb6646e611ea156a14b4e3e3305bd86acbd60
Olololya <ablazerogue@gmail.com>
2016-11-08 11:34:37 +0300

    35. Get random element

---
+++ task35.js
@@ -0,0 +1,10 @@
+//Write a JavaScript function to get a random item from an array.
+
+function getRandomElem(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+console.log( getRandomElem([254, 45, 212, 365, 2543]) );
+console.log( getRandomElem([254, 45, 212, 365, 2543]) );
+console.log( getRandomElem([254, 45, 212, 365, 2543]) );
+console.log( getRandomElem([254, 45, 212, 365, 2543]) );
