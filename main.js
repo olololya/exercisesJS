a3a48c066482d817fb269c5367b5c1160a3646bf
Olololya <ablazerogue@gmail.com>
2016-11-15 11:50:23 +0300

    2. Add simple animation

--- main.js
+++ main.js
@@ -3,14 +3,37 @@
 function draw() {
   let canvas = document.getElementById('canv');
 
+  let ctx = null;
   if (canvas.getContext) {
-    let ctx = canvas.getContext('2d');
-
-    ctx.fillStyle = 'rgb(200, 0, 0)';
-    ctx.strokeRect(0, 0, canvas.width, canvas.height);
-
+    ctx = canvas.getContext('2d');
   } else {
     throw new Error('Can\'t get context for canvas');
   }
+
+  ctx.fillStyle = 'rgb(101, 255, 204)';
+
+  var x = 50;
+  var speed = 1;
+
+  function animation() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+    ctx.beginPath();
+    ctx.arc(x, 70, 50, 0, Math.PI * 2, true);
+    ctx.fill();
+    ctx.stroke();
+
+    x += speed;
+    if (x > canvas.width - 50) {
+      speed = -speed;
+    }
+    if (x < 50) {
+      speed = -speed;
+    }
+    setTimeout(animation, 7);
+  }
+
+  animation();
 }
 
