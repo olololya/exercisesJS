f0a1721e993e93f42fc9d9925d33b781417e7c71
Olololya <ablazerogue@gmail.com>
2016-11-15 12:07:36 +0300

    4. Add random direction on start

--- main.js
+++ main.js
@@ -15,8 +15,10 @@ function draw() {
   let radius = 50;
   let x = (canvas.width / 2) - (radius / 2);
   let y = (canvas.height / 2) - (radius / 2);
-  let speed_x = 1;
-  let speed_y = 1;
+
+  let max = 1, min = -1;
+  let speed_x = Math.floor(Math.random() * (max - min + 1) + min) || max;
+  let speed_y = Math.floor(Math.random() * (max - min + 1) + min) || min;
 
   function animation() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
